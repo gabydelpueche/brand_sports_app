15ea1de11bbae328118864ec2df6d767cb101d3e
Gabriella Delpueche <gdelpu720@west-mec.org>
2024-05-15 14:10:18 -0700

    FIX: check time off function

--- public/check_time_off.js
+++ public/check_time_off.js
@@ -1,43 +1,31 @@
 const { database } = require("./db");
+const express = require("express");
 
 async function check_time_off(start, end, id){
     
     const records = await database.query("SELECT * FROM time_off WHERE worker = $1;", [id]);
 
     for(i = 0; i < records.rows.length; i++){
-        const time_start = records.rows[0].start_time;
-        const time_end = records.rows[0].end_time;
+        const time_start = records.rows[i].start_time;
+        const time_end = records.rows[i].end_time;
 
-        const check_time = await database.query("SELECT * check_time($1, $2, $3, $4) as within_range", [time_start, time_end, start.getTime(), end.getTime()]);
-        
-        if(check_time.rows[0].within_range == false){
+        let start_between = start.isAfter(time_start) && start.isBefore(time_end);
+        let end_between = end.isBefore(time_end) && end.isAfter(time_start);
+
+        if(start_between && end_between){
             return {
-                status: 200,
-                worker: data.rows[0].worker, 
-                time_available: true
+                status : 400,
+                available : 200,
+                message : "You have already requested time off during this time period",
+                reason : records.rows[i].reason,
+                time_period: `Between ${time_start} and ${time_end}`
             };
         };
+    };
 
-        if(check_time.rows[0].within_range == true) {
-            // retrieve date from db
-            // const start_day = new Date(data.rows[0].start_time);
-            // const end_day = new Date(data.rows[0].end_time);
-            // console.log("Start:" + start_day + "End:" + end_day);
-
-            // convert date to month & day
-            // const start_date = start_day.getDate();
-            // const end_date = start_end.getDate();
-            // console.log("New Start:" + start_date + "New End:" + end_date);
-
-            return {
-                status: 200,
-                worker: data.rows[0].worker,
-                time_available: false,
-                from: data.rows[0].start_time,
-                to: data.rows[0].end_time,
-                message: data.rows[0].reason
-            };
-        };
+    return {
+        status : 200,
+        available : true
     };
 };
 
